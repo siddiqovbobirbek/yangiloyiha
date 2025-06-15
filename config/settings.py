dcbecc36070eb9349792d454fc2aa4aa2b6274f4
Siddiqov Bobirbek <siddiqovbobirbekman@gmail.com>
2025-06-16 00:14:07 +0500

    allowed-hosts qo'shildi

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = env.str('SECRET_KEY', default=get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=[''])
 
 
 SITE_ID = 1
